908d8500c74fb775e4acde1762379faef0e3a952
Tame <tame@Eyoatams-MacBook-Pro.local>
2020-08-24 02:48:08 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,87 @@
+new Vue({
+  el: "#app",
+  data: {
+    playerHealth: 100,
+    monsterHealth: 100,
+    gameIsRunning: false,
+    turns: [],
+  },
+  methods: {
+    startGame() {
+      this.gameIsRunning = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.turns = [];
+    },
+    attack() {
+      let damage = this.calcDamage(3, 10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Player Hits Computer for" + damage,
+      });
+      if (this.checkWin()) {
+        return;
+      }
+
+      this.monsterAttacks();
+    },
+    specialAttack() {
+      var damage = this.calcDamage(10, 20);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Player Hits Hard Computer for" + damage,
+      });
+      if (this.checkWin()) {
+        return;
+      }
+      this.monsterAttacks();
+    },
+    heal() {
+      if (this.playerHealth <= 92) {
+        this.playerHealth += 8;
+      } else {
+        this.playerHealth = 100;
+      }
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Player Heals by 10",
+      });
+      this.monsterAttacks();
+    },
+    giveUp() {
+      this.gameIsRunning = false;
+    },
+    monsterAttacks() {
+      let damage = this.calcDamage(10, 20);
+      this.playerHealth -= this.calcDamage(5, 12);
+      this.checkWin();
+      this.turns.unshift({
+        isPlayer: false,
+        text: "Computer Hits Player" + damage,
+      });
+    },
+    calcDamage(min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    checkWin() {
+      if (this.monsterHealth <= 0) {
+        if (confirm("You won! New game?")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if (confirm("You Lost New Game?")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
+    },
+  },
+});
